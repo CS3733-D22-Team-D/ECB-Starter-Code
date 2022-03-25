69d6da2ae48d5f7c5062c7a7956bc38da4cf43fd
Chandler Garcia <crgarcia@wpi.edu>
2022-03-25 12:16:39 -0400

    Mortgage constructors made

--- src/main/java/edu/wpi/teamname/entity/Mortgage.java
+++ src/main/java/edu/wpi/teamname/entity/Mortgage.java
@@ -1,4 +1,18 @@
 package edu.wpi.teamname.entity;
 
 public class Mortgage {
+    private double amount;
+    private double rate;
+    private int years;
+
+    public Mortgage(double amount, double rate, int years){
+        this.amount = amount;
+        this.rate = rate;
+        this.years = years;
+    }
+    public Mortgage(){
+        this.amount = 0;
+        this.rate = 0;
+        this.years = 0;
+    }
 }
